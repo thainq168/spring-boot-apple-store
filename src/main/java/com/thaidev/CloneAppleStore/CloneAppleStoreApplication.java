66bb9d66966d48ac7f845ae213483888e8bb81da
thainq168 <19522189@gm.uit.edu.vn>
2023-12-20 00:04:53 +0700

    Project Initialization

---
+++ src/main/java/com/thaidev/CloneAppleStore/CloneAppleStoreApplication.java
@@ -0,0 +1,13 @@
+package com.thaidev.CloneAppleStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloneAppleStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloneAppleStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thaidev/CloneAppleStore/CloneAppleStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thaidev.CloneAppleStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloneAppleStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
