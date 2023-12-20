dac2ff3c032a3680b5aa7250d5f91ab7c6700151
thainq168 <19522189@gm.uit.edu.vn>
2023-12-20 13:30:17 +0700

    Update project

---
+++ src/main/java/com/thaidev/CloneAppleStore/controller/HomeController.java
@@ -0,0 +1,110 @@
+package com.thaidev.CloneAppleStore.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.thaidev.CloneAppleStore.dto.UserDTO;
+import com.thaidev.CloneAppleStore.global.GlobalData;
+import com.thaidev.CloneAppleStore.model.Role;
+import com.thaidev.CloneAppleStore.model.User;
+import com.thaidev.CloneAppleStore.service.CategoryService;
+import com.thaidev.CloneAppleStore.service.ProductService;
+import com.thaidev.CloneAppleStore.service.RoleService;
+import com.thaidev.CloneAppleStore.service.UserService;
+
+@Controller
+public class HomeController {
+    @Autowired
+    private PasswordEncoder bCryptPasswordEncoder;
+
+    @Autowired
+    UserService userService;
+
+    @Autowired
+    RoleService roleService;
+
+    @Autowired
+    CategoryService categoryService;
+
+    @Autowired
+    ProductService productService;
+
+    @GetMapping({"/", "/home"})
+    public String home(Model model){
+        model.addAttribute("cartCount", GlobalData.cart.size());
+        return "index";
+    } //index
+    @GetMapping("/users/add")
+    public String updateUser(Model model){
+        UserDTO currentUser = new UserDTO();
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        if (principal instanceof UserDetails && ((UserDetails) principal).getUsername() != null) {
+            String currentUsername = ((UserDetails)principal).getUsername();
+            User user = userService.getUserByEmail(currentUsername).get();
+            currentUser.setId(user.getId());
+            currentUser.setEmail(user.getEmail());
+            currentUser.setPassword("");
+            currentUser.setFirstName(user.getFirstName());
+            currentUser.setLastName(user.getLastName());
+            List<Integer> roleIds = new ArrayList<>();
+            for (Role item:user.getRoles()) {
+                roleIds.add(item.getId());
+            }
+            currentUser.setRoleIds(roleIds);
+        }//get current User runtime
+
+        model.addAttribute("userDTO", currentUser);
+        return "userRoleAdd";
+    }
+    @PostMapping("/users/add")
+    public String postUserAdd(@ModelAttribute("userDTO") UserDTO userDTO) {
+        //convert dto > entity
+        User user = new User();
+        user.setId(userDTO.getId());
+        user.setEmail(userDTO.getEmail());
+        user.setPassword(bCryptPasswordEncoder.encode(userDTO.getPassword()));
+        user.setFirstName(userDTO.getFirstName());
+        user.setLastName(userDTO.getLastName());
+        List<Role> roles = userService.getUserById(user.getId()).get().getRoles();
+        user.setRoles(roles);
+
+        userService.updateUser(user);
+        return "index";
+    }
+
+    @GetMapping("/shop")
+    public String shop(Model model){
+        model.addAttribute("cartCount", GlobalData.cart.size());
+        model.addAttribute("categories", categoryService.getAllCategory());
+        model.addAttribute("products", productService.getAllProduct());
+        return "shop";
+    } //view All Products
+
+    @GetMapping("/shop/category/{id}")
+    public String shopByCat(@PathVariable int id, Model model){
+        model.addAttribute("cartCount", GlobalData.cart.size());
+        model.addAttribute("categories", categoryService.getAllCategory());
+        model.addAttribute("products", productService.getAllProductByCategoryId(id));
+        return "shop";
+    } //view Products By Category
+
+    @GetMapping("/shop/viewproduct/{id}")
+    public String viewProduct(@PathVariable long id, Model model){
+        model.addAttribute("cartCount", GlobalData.cart.size());
+        model.addAttribute("product", productService.getProductById(id).get());
+        return "viewProduct";
+    } //view product Details
+
+
+}
---
+++ src/main/java/com/thaidev/CloneAppleStore/controller/LoginController.java
@@ -0,0 +1,67 @@
+package com.thaidev.CloneAppleStore.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.thaidev.CloneAppleStore.dto.UserDTO;
+import com.thaidev.CloneAppleStore.global.GlobalData;
+import com.thaidev.CloneAppleStore.model.Role;
+import com.thaidev.CloneAppleStore.model.User;
+import com.thaidev.CloneAppleStore.repository.RoleRepository;
+import com.thaidev.CloneAppleStore.repository.UserRepository;
+
+@Controller
+public class LoginController {
+    @Autowired
+    private PasswordEncoder bCryptPasswordEncoder;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @GetMapping("/login")
+    public String login(){
+        GlobalData.cart.clear();
+        return "login";
+    }//page login
+
+    @GetMapping("/forgotpassword")
+    public String forgotPass(Model model){
+        model.addAttribute("userDTO", new UserDTO());
+        return "forgotpassword";
+    }
+
+    @GetMapping("/register")
+    public String registerGet(Model model){
+        return "register";
+    } //page register
+
+    @PostMapping("/register")
+    public String registerPost(@ModelAttribute User userModel, HttpServletRequest request) throws ServletException{
+        //chuyen password tu form dki thanh dang ma hoa
+        String password = userModel.getPassword();
+        userModel.setPassword(bCryptPasswordEncoder.encode(password));
+        //set mac dinh role user,admin
+        List<Role> roles = new ArrayList<>();
+        roles.add(roleRepository.findById(1).get());
+        roles.add(roleRepository.findById(2).get());
+        userModel.setRoles(roles);
+        userRepository.save(userModel);
+        //login & chuyen den page home
+        request.login(userModel.getEmail(), password);
+        return "redirect:/";
+    }//after register success
+}
